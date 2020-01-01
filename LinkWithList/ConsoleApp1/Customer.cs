d54fb37a61f01e6740796154a3d34d7f7aab8500
Jennifer Faron <jf891w@gmail.com>
2020-01-01 14:07:54 -0600

    LinkWithList

---
+++ LinkWithList/ConsoleApp1/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkWithList
+{
+   class Customer
+   {
+      public string First { get; set; }
+      public string Last { get; set; }
+      public string state;
+
+      public string State
+      {
+         get
+         {
+            return state;
+         }
+         set
+         {
+            if (value == "Canada")
+               throw new Exception("This is not a US State");
+            else
+               state = value;
+         
+         }
+      }
+
+
+   }
+}
---
+++ LinkWithList/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkWithList
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         //data source for customers
+         List<Customer> customers = new List<Customer>
+         {
+            new Customer{ First= "Joe", Last= "Smith", State = "Missouri" },
+            new Customer{ First= "Betty", Last= "Jones", State = "California" },
+            new Customer{ First= "Jane", Last= "Green", State = "California" },
+            new Customer{ First= "Bobby", Last = "Brown", State = "California" },
+            new Customer{ First= "Linda", Last="Avery", State="Alaska" } };
+
+         //query 1- customers only
+         IEnumerable<string> stateQuery =
+            from cust in customers
+            where cust.State == "California"
+            select cust.First + " " + cust.Last;
+
+         foreach(var a in stateQuery)
+         {
+            Console.WriteLine("Customer name is: {0}", a);
+         }
+
+         Console.ReadKey();
+
+         
+
+      
+      }
+   }
+}
